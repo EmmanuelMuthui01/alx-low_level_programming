e7178f7e51f98ff64a4180df335153c39f2393ac
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-06-15 16:35:23 -0700

    Print a quote to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
 
 int main()
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	write(STDOUT_FILENO, message, strlen(message));
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59;)
+	
 	return (1);
