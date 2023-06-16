54410bfc7b5473ea94e3c53b2af9c97b3bdab758
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-06-16 14:14:16 -0700

    print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,18 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 57;)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
-		n++;
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar('$');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
