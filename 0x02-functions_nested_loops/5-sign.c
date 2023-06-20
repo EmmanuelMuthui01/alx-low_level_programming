63fc827c2a9eb05b00aabe563a559ffd1ba7a8ec
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-06-20 14:39:36 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,17 @@
  * print_sign - prints the sign of a number
  * @n: the character to be printed
  *
- * Return: 1 (success)
+ * Return: 1 if n is greater than zero, 0 if zero, -1 if less than zero
  */
 
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n < 48)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
