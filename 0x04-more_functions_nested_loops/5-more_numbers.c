fbdefa2d5def3bb68c50086db605bfce25f51b3d
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-06-23 06:26:36 -0700

    Corrections to the loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,15 @@
 
 void more_numbers(void)
 {
-	int i;
-	char num;
+	int i, num;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num < 10)
-			{
-				_putchar(num + '0');
-			}
-			else
-			{
-				_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');
-			}
+			if (num >= 10)
+				_putchar('1');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
