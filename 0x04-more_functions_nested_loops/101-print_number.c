4b052e87989eaf9c6a5cfa1982d12933d2ad7dd2
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-06-24 04:41:26 -0700

    Print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(n + '0');
+		num = -num;
 	}
-	else if ((n / 10) > 0)
-	{
-		_putchar(n + '0');
-	}
-	_putchar('\n');
+
+	if ((num / 10) >  0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
