9f32a000e325664621f78277b92d63c503245041
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-07-01 15:49:06 -0700

    First 50 Fibonacci series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,20 +12,20 @@ int main(void)
 	long int fibonacci[50];
 
 	fibonacci[0] = 1;
-	fibonacci[1] = ';
+	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
 	for(i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 
-		if (i ==49)
+		if (i == 49)
 		{
-			printf("ld\n", fibonacci[i]);
+			printf("%ld\n", fibonacci[i]);
 		}
 		else
 		{
-			printf("%ld\n, ", );
+			printf("%ld, ", fibonacci[i]);
 		}
 	}
 	return (0);
