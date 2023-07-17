1f59f853fd4e74a99d063598fb5bd1ea183f5a5e
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-07-17 03:58:24 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 if it returns name of the file it was compiled from.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
