caa39d9af67ae85a63c003efa80e38fdff5a5c93
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-07-19 19:57:02 -0700

    Performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,4 @@
-#include "3-calc.h"i
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include "3-calc.h"
 
 /**
  * op_add - Function that adds two integers 'a' and 'b'.
