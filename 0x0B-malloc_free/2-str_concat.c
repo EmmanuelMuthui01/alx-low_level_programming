9e12e6898dbfcb7bfc3050307af10b3fa9f557f8
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-08-08 16:58:47 -0700

    Concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *result;
-	int len1 = strlen(s1); /*get length of string one (s1)*/
-	int len2 = strlen(s2); /*get length of string two (s2)*/
-	int length = (len1 + len2 + 1); /*Length of s1, s2, & '\0'*/
+	char *result; /*pointer to newly allocated memory*/
+	int length = 0; /*stores length of concatenated string*/
+	/*int len1 = strlen(s1); get length of string one (s1)*/
+	/*int len2 = strlen(s2); get length of string two (s2)*/
 
 	if (s1 == NULL)
 	{
@@ -28,14 +28,18 @@ char *str_concat(char *s1, char *s2)
 		s2 = ""; /*If NULL is passed in s2, treate it as empty string*/
 	}
 
+	length = (strlen(s1) + strlen(s2) + 1);
 	result = malloc(length); /*allocate memory for new concatenated string*/
+
 	if (result == NULL) /*If malloc returns NULL ptr, return NULL*/
 	{
 		return (NULL);
 	}
-
-	strcpy(result, s1); /*Copy contents of s1 to result*/
-	strcat(result, s2); /*concatenate contents of s2 to s1 in result*/
-
+	else
+	{
+		strcpy(result, s1);
+		strcat(result, s2);
+	}
 	return (result); /*return the concatenated string*/
+	free(result);
 }
