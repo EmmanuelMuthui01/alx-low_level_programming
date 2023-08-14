e0f95ab83588e4e1b9b96ad08565a837f6ac9b6b
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-08-14 06:38:19 -0700

    Print name of __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
