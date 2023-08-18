99973a3448917422a79159db280e82e3bcc4564b
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-08-18 14:17:32 -0700

    Prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,14 +3,14 @@
 /**
  * print_all - prints anything.
  * @format: is a list of types of arguments passed to the function
- * i = int; c = char; f = float; s = char *;
+ *
  * Return: nothing.
  */
 
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *str, *separator = ", ";
+	char *str, *separator = "";
 	va_list my_list;
 
 	va_start(my_list, format);
@@ -22,9 +22,31 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-				printf("%s%c", separator, va_arg(my_list, int);
-				
+				printf("%s%c", separator, va_arg(my_list, int));
 				break;
 
 				case 'i':
-				printf("%s%c", separator, va_arg(my_list, int);
+				printf("%s%d", separator, va_arg(my_list, int));
+				break;
+
+				case 'f':
+				printf("%s%f", separator, va_arg(my_list, double));
+				break;
+
+				case 's':
+				str = va_arg(my_list, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+				default:
+				i++;
+				continue;
+			}
+			separator = ",";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(my_list);
+}
