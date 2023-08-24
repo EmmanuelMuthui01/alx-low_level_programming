94b490560d7eda30bf76f7ac7a63feccac2a27ee
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-08-24 15:57:17 -0700

    Header files

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,9 +7,17 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+
+int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
+
+int is_divisible(int num, int div);
 int is_prime_number(int n);
+
+int find_string_length(char *s);
+int check_if_palindrome(char *s, int length, int index);
 int is_palindrome(char *s);
+
 int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H is defined*/
