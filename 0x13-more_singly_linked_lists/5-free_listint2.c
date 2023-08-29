1c70d4e15b93ab9315acc5a3c986e56d96140f54
EmmanuelMuthui01 <emmanuelmuthui14@gmail.com>
2023-08-29 11:33:19 -0700

    frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,19 +7,18 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current_node = *head; /*Create a pointer to traverse the list*/
 	listint_t *temp_ptr; /*Create another pointer to temporarily store next node*/
 
-	if (head == NULL || *head == NULL) /*check if head pointer is null*/
+	if (head == NULL) /*check if head pointer is null*/
 	{
-		return; /*There is nothing to return*/
+		return;
 	}
 
-	while (current_node != NULL) /*Traverse until end of the list*/
+	while (*head != NULL) /*Traverse until end of the list*/
 	{
-		temp_ptr = current_node->next; /*Store next node to prent losing it*/
-		free(current_node); /*Free current node*/
-		current_node = temp_ptr; /*Move to the next node*/
+		temp_ptr = (*head)->next; /*Store the next node temporary*/
+		free(*head); /*Free current node*/
+		*head = temp_ptr; /*Move the head pointer to the next node*/
 	}
-	*head = NULL; /*Set the head pointer to NULL to indicate an empty list*/
+	head = NULL; /*Set the head pointer to NULL to indicate an empty list*/
 }
